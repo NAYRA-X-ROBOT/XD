0c6f1a855991b772b17ded82134d132022f4b0ea
Heart Beart Music <154865461+HEART-X-BEAT@users.noreply.github.com>
2024-05-14 19:00:08 +0530

    Update ping.py

--- DAXXMUSIC/plugins/tools/ping.py
+++ DAXXMUSIC/plugins/tools/ping.py
@@ -17,7 +17,7 @@ from config import BANNED_USERS
 async def ping_com(client, message: Message, _):
     start = datetime.now()
     response = await message.reply_video(
-        video="https://graph.org/file/5690109178f081adf464d.mp4",
+        video="https://telegra.ph/file/e67b9a02c18bef5b0c37e.jpg",
         caption=_["ping_1"].format(app.mention),
     )
     pytgping = await DAXX.ping()
